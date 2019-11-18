8d94c85dee6b9dd67497b76ff2c2ae073f80cf72
Sean Good <tree662@protonmail.com>
2019-11-18 01:12:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
+class Shoe
+  attr_accessor :color :size :material 
+  attr_reader :condition
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  
+  def cobble
+    shoe_condition == "new"
+    puts "Your shoe is good as new!"
+  end
+end
+
+  
 # Make your shoe class here!

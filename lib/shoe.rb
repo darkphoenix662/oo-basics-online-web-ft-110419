65dd14451095de097b412e06167b437d09d272b0
Sean Good <tree662@protonmail.com>
2019-11-18 01:14:20 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 class Shoe
-  attr_accessor :color :size :material :condition
-  attr_reader :brand
+  # attr_accessor :color :size :material :condition
+  # attr_reader :brand
   
-  def initialize(brand)
-    @brand = brand
-  end
+  # def initialize(brand)
+  #   @brand = brand
+  # end
   
   # def cobble(shoe_condition)
   #   if shoe_condition == "new"

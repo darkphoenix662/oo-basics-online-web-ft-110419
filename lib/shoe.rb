8c45e0d5df48029a0c9ce621a36e6d20fe5f2341
Sean Good <tree662@protonmail.com>
2019-11-18 01:14:58 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,9 @@ class Shoe
   # attr_accessor :color :size :material :condition
   # attr_reader :brand
   
-  # def initialize(brand)
-  #   @brand = brand
-  # end
+  def initialize(brand)
+    @brand = brand
+  end
   
   # def cobble(shoe_condition)
   #   if shoe_condition == "new"
